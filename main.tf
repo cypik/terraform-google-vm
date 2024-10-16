f960ef569c883b0fb8adf629bb325493edd4481a
cypiksuresh <suresh.y@cypik.com>
2024-10-16 18:09:57 +0530

    update resource and resdme.md

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "labels" {
   source      = "cypik/labels/google"
-  version     = "1.0.1"
+  version     = "1.0.2"
   name        = var.name
   environment = var.environment
   label_order = var.label_order
   managedby   = var.managedby
   repository  = var.repository
+  extra_tags  = var.extra_tags
 }
 
-data "google_client_config" "current" {
-}
+data "google_client_config" "current" {}
 
 #####==============================================================================
 ##### Manages a VM instance resource within GCE.
@@ -20,38 +20,108 @@ data "google_client_config" "current" {
 #tfsec:ignore:google-compute-enable-shielded-vm-vtpm
 #tfsec:ignore:google-compute-enable-shielded-vm-im
 #tfsec:ignore:google-compute-vm-disk-encryption-customer-key
-resource "google_compute_instance" "default" {
+resource "google_compute_instance" "vm_instance" {
   count        = var.create_instances && var.instance_count > 0 ? var.instance_count : 0
   name         = format("%s-vm-%02d", module.labels.id, count.index + 1)
   machine_type = var.machine_type
-  zone         = var.gcp_zone
+  zone         = var.zone
   tags         = var.instance_tags
   project      = data.google_client_config.current.project
+
   boot_disk {
+    auto_delete = true
+    device_name = "boot-disk"
+    mode        = "READ_WRITE"
+
     initialize_params {
-      image = var.image
-      labels = {
-        my_label = "value"
-      }
+      size                        = var.boot_disk_size
+      type                        = var.boot_disk_type
+      image                       = var.image
+      labels                      = { environment = "production" }
+      enable_confidential_compute = false
+    }
+  }
+
+  dynamic "scratch_disk" {
+    for_each = var.local_disks ? [1] : [] // Create scratch disk if local_disks is true
+
+    content {
+      interface = "NVME"
+    }
+  }
+  dynamic "shielded_instance_config" {
+    for_each = var.enable_shielded_instance_config ? [1] : []
+    content {
+      enable_secure_boot          = lookup(var.shielded_instance_config, "enable_secure_boot", false)
+      enable_vtpm                 = lookup(var.shielded_instance_config, "enable_vtpm", true)
+      enable_integrity_monitoring = lookup(var.shielded_instance_config, "enable_integrity_monitoring", true)
+    }
+  }
+
+  dynamic "network_performance_config" {
+    for_each = var.enable_network_performance_config ? [1] : []
+    content {
+      total_egress_bandwidth_tier = var.total_egress_bandwidth_tier
     }
   }
 
   network_interface {
+    network    = var.network
     subnetwork = var.subnetwork
 
     dynamic "access_config" {
       for_each = var.enable_public_ip ? [1] : []
       content {
-        # Add access_config settings here if needed
+        nat_ip                 = var.nat_ip
+        network_tier           = var.network_tier
+        public_ptr_domain_name = var.public_ptr_domain_name
       }
     }
-  }
-  metadata                = var.metadata
-  metadata_startup_script = var.metadata_startup_script
 
-  service_account {
-    email  = var.service_account_email
-    scopes = var.service_account_scopes
+    dynamic "ipv6_access_config" {
+      for_each = var.enable_ipv6 ? [1] : []
+      content {
+        external_ipv6 = var.external_ipv6
+        name          = "External IPv6"
+        network_tier  = "STANDARD"
+      }
+    }
+
+    dynamic "alias_ip_range" {
+      for_each = var.enable_alias_ip_range ? [1] : []
+      content {
+        ip_cidr_range         = "192.168.0.0/24"
+        subnetwork_range_name = "my-secondary-range"
+      }
+    }
+
+    nic_type    = "GVNIC"
+    stack_type  = "IPV4_ONLY"
+    queue_count = 1
   }
+
+  dynamic "service_account" {
+    for_each = var.enable_service_account ? [1] : []
+    content {
+      email  = var.service_account_email
+      scopes = var.service_account_scopes
+    }
+  }
+
+  dynamic "guest_accelerator" {
+    for_each = var.enable_accelerator ? [1] : []
+    content {
+      type  = var.accelerator_type  # e.g., "nvidia-tesla-k80"
+      count = var.accelerator_count # e.g., 2
+    }
+  }
+
+  metadata                  = var.metadata
+  metadata_startup_script   = var.metadata_startup_script
   allow_stopping_for_update = var.allow_stopping_for_update
+  can_ip_forward            = var.can_ip_forward
+  description               = var.description
+  desired_status            = var.desired_status
+  enable_display            = var.enable_display
+  hostname                  = var.custom_hostname
 }
--- variables.tf
+++ variables.tf
@@ -18,8 +18,14 @@ variable "label_order" {
 
 variable "managedby" {
   type        = string
-  default     = "cypik"
-  description = "ManagedBy, eg 'cypik'."
+  default     = "info@cypik.com"
+  description = "ManagedBy, e.g. 'info@cypik.com'."
+}
+
+variable "extra_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags for the resource."
 }
 
 variable "repository" {
@@ -40,13 +46,19 @@ variable "machine_type" {
   description = "Machine type to create, e.g. n1-standard-1"
 }
 
-variable "gcp_zone" {
+variable "zone" {
   type        = string
   default     = ""
   description = "The GCP zone to create resources in"
 }
 
 variable "subnetwork" {
+  type        = string
+  default     = ""
+  description = "Subnet to deploy to. Only one of network or subnetwork should be specified."
+}
+
+variable "network" {
   type        = string
   default     = ""
   description = "Subnet to deploy to. Only one of network or subnetwork should be specified."
@@ -62,18 +74,6 @@ variable "metadata_startup_script" {
   type        = string
   default     = ""
   description = "User startup script to run when instances spin up"
-}
-
-variable "service_account_email" {
-  type        = string
-  default     = ""
-  description = "The service account e-mail address. "
-}
-
-variable "service_account_scopes" {
-  type        = list(string)
-  default     = []
-  description = "A list of service scopes."
 }
 
 variable "metadata" {
@@ -109,4 +109,225 @@ variable "instance_count" {
 output "instance_count_output" {
   description = "The value of the instance_count variable."
   value       = var.instance_count
+}
+
+
+#variable "disk_encryption_key_raw" {
+#  description = "Raw disk encryption key for the instance."
+#  type        = string
+#  default     = "your_raw_encryption_key_here"
+#}
+
+#variable "kms_key_self_link" {
+#  description = "Self link of the KMS key."
+#  type        = string
+#  default     = "projects/my-project/locations/us-central1/keyRings/my-key-ring/cryptoKeys/my-key"
+#}
+
+
+
+#variable "data_disk_size" {
+#  description = "Size of the data disk in GB."
+#  type        = number
+#  default     = 50
+#}
+#
+#variable "region" {
+#  description = "Region for the VM IP."
+#  type        = string
+#  default     = "us-central1"
+#}
+
+
+
+variable "enable_service_account" {
+  description = "Enable or disable the service account for the instance"
+  type        = bool
+  default     = false
+}
+
+
+
+#variable "vm_machine_type" {
+#  description = "Machine type for the VM instance"
+#  type        = string
+#  default     = "e2-medium"
+#}
+
+#variable "vm_zone" {
+#  description = "Zone for the VM instance"
+#  type        = string
+#  default     = "us-central1-a"
+#}
+
+
+variable "boot_disk_size" {
+  description = "Boot disk size in GB"
+  type        = number
+  default     = 20
+}
+
+variable "boot_disk_type" {
+  description = "Boot disk type"
+  type        = string
+  default     = "pd-standard"
+}
+
+#variable "enable_partner_metadata" {
+#  description = "Enable partner metadata"
+#  type        = bool
+#  default     = false
+#}
+
+#variable "partner_metadata_key" {
+#  description = "Partner metadata key"
+#  type        = string
+#  default     = ""
+#}
+
+#variable "partner_metadata_value" {
+#  description = "Partner metadata value"
+#  type        = any
+#  default     = {}
+#}
+
+variable "enable_shielded_instance_config" {
+  description = "Enable shielded instance config"
+  type        = bool
+  default     = false
+}
+
+variable "shielded_instance_config" {
+  description = "Shielded instance configuration options"
+  type        = map(bool)
+  default = {
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+    enable_integrity_monitoring = true
+  }
+}
+
+#variable "data_disk_name" {
+#  description = "Name of the attached data disk"
+#  type        = string
+#  default     = "data-disk"
+#}
+
+variable "enable_network_performance_config" {
+  description = "Enable network performance configuration"
+  type        = bool
+  default     = false
+}
+
+variable "total_egress_bandwidth_tier" {
+  description = "Egress bandwidth tier"
+  type        = string
+  default     = "STANDARD"
+}
+
+
+
+
+
+variable "nat_ip" {
+  description = "NAT IP address for public access"
+  type        = string
+  default     = ""
+}
+
+variable "network_tier" {
+  description = "Network tier for the public IP"
+  type        = string
+  default     = "STANDARD"
+}
+
+variable "public_ptr_domain_name" {
+  description = "Public PTR domain name for the instance"
+  type        = string
+  default     = ""
+}
+
+
+
+variable "enable_ipv6" {
+  description = "Enable IPv6 access configuration"
+  type        = bool
+  default     = false
+}
+
+variable "external_ipv6" {
+  description = "External IPv6 address"
+  type        = string
+  default     = ""
+}
+
+variable "enable_alias_ip_range" {
+  description = "Enable alias IP range"
+  type        = bool
+  default     = false
+}
+
+variable "service_account_email" {
+  description = "Service account email"
+  type        = string
+  default     = ""
+}
+
+variable "service_account_scopes" {
+  description = "Service account scopes"
+  type        = list(string)
+  default     = ["https://www.googleapis.com/auth/cloud-platform"]
+}
+
+variable "enable_display" {
+  description = "Enable display for the instance"
+  type        = bool
+  default     = false
+}
+
+variable "can_ip_forward" {
+  description = "Allow IP forwarding"
+  type        = bool
+  default     = false
+}
+
+variable "description" {
+  description = "Description for the instance"
+  type        = string
+  default     = "VM instance created with Terraform"
+}
+
+variable "custom_hostname" {
+  description = "Custom hostname for the VM instance (RFC-1035 compliant)"
+  type        = string
+  default     = ""
+}
+
+variable "enable_accelerator" {
+  description = "Enable GPU accelerator"
+  type        = bool
+  default     = false
+}
+
+variable "accelerator_type" {
+  description = "Type of GPU accelerator"
+  type        = string
+  default     = "nvidia-tesla-k80"
+}
+
+variable "accelerator_count" {
+  description = "Count of GPU accelerators"
+  type        = number
+  default     = 1
+}
+
+variable "desired_status" {
+  type    = string
+  default = "RUNNING"
+}
+
+variable "local_disks" {
+  description = "Flag to determine if scratch disk should be created"
+  type        = bool
+  default     = false // Set the default value as per your requirement
 }
